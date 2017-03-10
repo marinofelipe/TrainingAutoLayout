33303c9dc8b1f7b4a16b510543283ab43ae242f9
Felipe Marino <felipemarino91@gmail.com>
2017-03-10 18:18:08 -0300

    Ended CircleView with layer as CAShapeLayer

--- AutoLayoutStudyProject/CircleView.swift
+++ AutoLayoutStudyProject/CircleView.swift
@@ -11,4 +11,44 @@ import UIKit
 @IBDesignable
 class CircleView: UIView {
 
+    @IBInspectable
+    public var fillColor: UIColor {
+        didSet {
+            self.layoutSubviews()
+            self.layoutIfNeeded()
+        }
+    }
+    
+    public override init(frame: CGRect) {
+        self.fillColor = UIColor.lightText
+        
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.clear
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        self.fillColor = UIColor.lightText
+        
+        super.init(coder: aDecoder)
+        self.backgroundColor = UIColor.clear
+    }
+    
+    public var shapeLayer: CAShapeLayer {
+        get {
+            return self.layer as! CAShapeLayer
+        }
+    }
+    
+    override class var layerClass: AnyClass {
+        return CAShapeLayer.self
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let bezierPath = UIBezierPath(ovalIn: self.bounds)
+        
+        self.shapeLayer.fillColor = self.fillColor.cgColor
+        self.shapeLayer.path = bezierPath.cgPath
+    }
 }
