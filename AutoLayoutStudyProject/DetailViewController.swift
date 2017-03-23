7fdfdeb4b33cf9bb5832714c78f3c6a2bfab53b7
Felipe Lefèvre Marino <marinofelipe@Felipes-MacBookPro.local>
2017-03-22 23:36:13 -0300

    Added Will layout subviews

--- AutoLayoutStudyProject/DetailViewController.swift
+++ AutoLayoutStudyProject/DetailViewController.swift
@@ -14,7 +14,12 @@ class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.vennDiagram.weight = .Left
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        print("Will layout subviews")
     }
     
     @IBAction func leftTapped(sender: UIButton) {
